72a95658165e58e0bd69c21d6bbf2cc556c67f05
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-07-17 19:20:50 -0400

    :sparkles: max/min & mean/std overloads

--- array-operations.jl
+++ array-operations.jl
@@ -2,6 +2,7 @@ import Pkg
 Pkg.activate(".")
 using Revise
 using SimpleSDMLayers # dev version
+using Statistics
 
 temperature = worldclim(1)
 precipitation = worldclim(12)
@@ -16,9 +17,30 @@ extrema(temperature)
 broadcast(abs, temperature)
 maximum(abs(temperature))
 
-## Max-min
-min(temperature, precipitation)
-max(temperature, precipitation)
-reduce(max, wc_vars)
+# Max-min
+@time min(temperature, precipitation)
+@time max(temperature, precipitation)
+@time test = reduce(max, wc_vars)
 wc_vars_resp = convert.(SimpleSDMResponse, wc_vars)
-reduce(max, wc_vars_resp)
+@time test = reduce(max, wc_vars_resp)
+
+minifloat = SimpleSDMPredictor([-1 0 1 nothing])
+miniint = SimpleSDMResponse([-1.0 0.0 1.0 nothing])
+mini32 = SimpleSDMPredictor([Float32.([-1.0 0.0 1.0])... nothing])
+min(miniint, minifloat)
+@time reduce(min, [miniint, minifloat, mini32])
+
+# Mean-std
+mean(wc_vars)
+std(wc_vars)
+# Attempt 1
+mininothing = SimpleSDMPredictor([nothing 1.0 nothing nothing])
+coordsnothing = map(x -> findall(isnothing, x.grid), [minifloat, miniint, mini32, mininothing])
+uniquenothing = unique(coordsnothing)
+unique(reduce(vcat, uniquenothing)) |> sort
+# Need InvertedIndex...
+# Attempt 2
+coordsnotnothing = map(x -> findall(!isnothing, x.grid), [minifloat, miniint, mini32, mininothing])
+uniquenot = unique(coordsnotnothing)
+@time reduce(intersect, uniquenot)
+@time intersect(uniquenot...)
