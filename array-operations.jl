5ff1f0dd613680550547cb5154e5b278038f66e0
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-07-17 15:28:39 -0400

    :sparkles: extrema & broadcast overloads

--- array-operations.jl
+++ array-operations.jl
@@ -4,3 +4,21 @@ using Revise
 using SimpleSDMLayers # dev version
 
 temperature = worldclim(1)
+precipitation = worldclim(12)
+wc_vars = worldclim(1:19)
+
+# Extrema overload
+minimum(temperature)
+maximum(temperature)
+extrema(temperature)
+
+# Broadcast type
+broadcast(abs, temperature)
+maximum(abs(temperature))
+
+## Max-min
+min(temperature, precipitation)
+max(temperature, precipitation)
+reduce(max, wc_vars)
+wc_vars_resp = convert.(SimpleSDMResponse, wc_vars)
+reduce(max, wc_vars_resp)
