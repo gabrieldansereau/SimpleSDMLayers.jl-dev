b48e395955520dca2b9aefc34392af1bec957d0b
gabrieldansereau <gabriel.dansereau@hotmail.com>
2020-07-20 16:37:53 -0400

    :sparkles: clip overload

--- dataframes.jl
+++ dataframes.jl
@@ -34,4 +34,24 @@ temperature[df]
 # Setindex
 tmp = copy(convert(SimpleSDMResponse, temperature))
 tmp[df]
-tmp[df] = Float32.(collect(1:nrow(df))) # let's put it aside for now
+# tmp[df] = Float32.(collect(1:nrow(df))) # let's put it aside for now
+
+# Inconsistent use of missing/nothing/NaN in GBIF code
+using GBIF
+kingfisher = GBIF.taxon("Megaceryle alcyon", strict=true)
+kf_occurrences = occurrences(kingfisher)
+for i in 1:9
+  occurrences!(kf_occurrences)
+end
+tmp = [k.latitude for k in kf_occurrences]
+sort(tmp) # none missing
+tmp = layer[kf_occurrences]
+sort(tmp) # none missing
+temperature_clip = clip(temperature, kf_occurrences)
+# whatever...
+
+# Clip
+temperature_clip = clip(temperature, df)
+temperature_clip.grid
+# using Plots
+# plot(temperature_clip)
