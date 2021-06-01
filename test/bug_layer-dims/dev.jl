201ef559e49b44cdd70fad3fb4cca7ee2af1bb92
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-06-01 15:51:52 -0400

    🚧 reduce allocations in matching functions

--- test/bug_layer-dims/dev.jl
+++ test/bug_layer-dims/dev.jl
@@ -175,3 +175,51 @@ RT = T <: SimpleSDMResponse ? SimpleSDMResponse : SimpleSDMPredictor
 # Compare allocation for range & LinRange
 @time LinRange(layer.bottom+stride(layer, 2), layer.top-stride(layer, 2), size(layer,1))
 @time range(layer.bottom+stride(layer, 2), layer.top-stride(layer, 2), length=size(layer,1))
+# Let's just switch it to range instead of LinRange
+
+## Step 3: Update match_longitude for range not LinRange ####
+
+# Load things
+layer = SimpleSDMPredictor(WorldClim, BioClim, 1)
+layer_subset = layer[coords]
+size(layer_subset) # should be 330, 570
+left, right, bottom, top = coords
+import SimpleSDMLayers: _match_latitude, _match_longitude
+
+# Preliminary tests
+lon = left
+@time _match_longitude(layer, left; side=:left) # 15k alloc
+ldiff = abs.(lon .- longitudes(layer))
+@time any(x -> isapprox(x, stride(layer, 1)), ldiff)
+@time any(x -> isequal(x, stride(layer, 1)), ldiff) # not much difference
+@time findlast(x -> isapprox(x, stride(layer, 1)), ldiff)
+@time findlast(x -> isequal(x, stride(layer, 1)), ldiff) # not much difference either
+@time findfirst(x -> isequal(x, stride(layer, 1)), ldiff)
+@time findall(x -> isequal(x, stride(layer, 1)), ldiff)
+@time filter(x -> x == stride(layer, 1), ldiff)
+@time lon in latitudes(layer)
+@time lon in ldiff
+@time any(isequal.(ldiff, stride(layer, 1)))
+@time any(isapprox.(ldiff, stride(layer, 1)))
+@time isequal.(ldiff, stride(layer,1))
+# With isequal.
+@time _match_longitude(layer, left; side=:left) # 13k
+@time _match_longitude(layer, left; side=:none) # 29 alloc (no change)
+# with lequal
+@time _match_longitude(layer, left; side=:left) # 39 alloc
+@time _match_longitude(layer, left; side=:none) # 40 alloc
+@time _match_longitude(layer, left; side=:right) # 39 alloc
+@time _match_longitude(layer, left+0.001; side=:right) # 41 alloc
+# with lapprox
+@time _match_longitude(layer, left; side=:left) # 39 alloc
+@time _match_longitude(layer, left; side=:none) # 40 alloc
+@time _match_longitude(layer, left; side=:right) # 39 alloc
+@time _match_longitude(layer, left+0.001; side=:right) # 41 alloc
+# Check with latitude
+@time _match_latitude(layer, bottom; side=:none) # 40 alloc
+@time _match_latitude(layer, bottom; side=:bottom) # 39 alloc
+@time _match_latitude(layer, top; side=:top) # 39 alloc
+@time _match_latitude(layer, top+0.001; side=:top) # 41 alloc
+# Check getindex result
+@time layer[coords] # 278 alloc!
+# compared to 47.69 k with previous fix, and 222 alloc before previous fix
