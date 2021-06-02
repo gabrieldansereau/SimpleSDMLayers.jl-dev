84d498f6e9d150066ee23c323e1eae3f8ae24c7a
Gabriel Dansereau <gabriel.dansereau@umontreal.ca>
2021-06-02 17:42:07 -0400

    🚧 use missing instead of nothing in DataFrames overload

--- test/bug_layer-dims/dev.jl
+++ test/bug_layer-dims/dev.jl
@@ -592,3 +592,35 @@ l5 = SimpleSDMPredictor(WorldClim, BioClim, 1; left=0.0, right=10.0, bottom=10.0
 l6 = SimpleSDMPredictor(WorldClim, BioClim, 1; left=0.0, right=10.0, bottom=20.0, top=30.0);
 vcat(l4, l5)
 vcat(l4, l6)
+
+## Step 8 : DataFrames no nothing ####
+
+# For issue
+using SimpleSDMLayers
+using DataFrames
+
+layer = SimpleSDMPredictor(WorldClim, BioClim, 1)
+
+df = DataFrame([layer, layer])
+allowmissing!(df)
+for col in [:x1, :x2]
+    replace!(df[!, col], nothing => missing)
+end
+dropmissing(df, [:x1, :x2])
+
+# Tests
+DataFrame(layer)
+DataFrame([layer, layer])
+
+l1 = layer[top = -89.0]
+typeof(l1)
+eltype(l1)
+typeof(l1.grid)
+eltype(l1.grid)
+
+df1 = DataFrame(l1)
+typeof(df1.values)
+eltype(df1.values)
+
+layers = [layer, layer]
+layers = [temperature_clip, temperature_clip]
